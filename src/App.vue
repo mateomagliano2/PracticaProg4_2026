fb8ec2d5b96ac0d516b6f2b8d46eea3a1180d692
mateomagliano2 <mmagliano@ddvs.edu.ar>
2026-08-20 21:40:29 -0300

    Se crea componente para el manejo de pags

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BlogPost from './components/BlogPost.vue';
 import { ref } from 'vue';
+import PaginatePost from './components/PaginatePost.vue';
 // const posts = ref([
 //   { id: 1, title: 'Post 1', body: 'RIVER' },
 //   { id: 2, title: 'Post 2', body: 'RIVER 1' },
@@ -17,6 +18,18 @@ const favorito = ref('');
 const cambiarFavorito = (title) => {
   favorito.value = title;
 }
+
+const inicio = ref(0);
+const fin = ref(pageXpost);
+const pageXpost = 5;
+const next = () => {
+  inicio.value = inicio.value + pageXpost;
+  fin.value = fin.value + pageXpost;
+}
+const prev = () => {
+  inicio.value = inicio.value - pageXpost;
+  fin.value = fin.value - pageXpost;
+}
 </script>
 
 
@@ -24,9 +37,14 @@ const cambiarFavorito = (title) => {
 
   <div class="container">
     <h1>Hola Maquina</h1>
-    <h2>{{ favorito }}</h2>
-    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body" class="mb-2"
-      @CambiarFavoritoNombre=cambiarFavorito />
+    <PaginatePost class="mb-2" @next="next" @prev="prev" />
+    <hr>
+    <h2>{{ favorito || "Ninguno" }}</h2>
+    <button class="btn btn-outline-primary" @click="prev">Prev</button>
+    <button class="btn btn-outline-primary" @click="next">Next</button>
+    <hr>
+    <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id"
+      :body="post.body" class="mb-2" @CambiarFavoritoNombre=cambiarFavorito />
   </div>
 
 </template>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineEmits(['next', 'prev']);
+</script>
+<template>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button @click="$emit('prev')" class="btn btn-outline-primary">Prev</button>
+        <button @click="$emit('next')" class="btn btn-outline-primary">Next</button>
+    </div>
+</template>

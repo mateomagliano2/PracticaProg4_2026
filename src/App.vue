cd46bf73657807bd7908125c30337edb90e28b56
mateomagliano2 <mmagliano@ddvs.edu.ar>
2026-08-20 21:10:41 -0300

    Se levanta una API y se muestra con componentes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <script setup>
+import BlogPost from './components/BlogPost.vue';
+import { ref } from 'vue';
+// const posts = ref([
+//   { id: 1, title: 'Post 1', body: 'RIVER' },
+//   { id: 2, title: 'Post 2', body: 'RIVER 1' },
+//   { id: 3, title: 'Post 3', body: 'Murieron en el 2018' },
+//   { id: 4, title: 'Post 4' }
+// ]);
+const posts = ref([])
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => posts.value = data)
 
+
+const favorito = ref('');
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+}
 </script>
 
 
 <template>
-  <h1>Hola Mat</h1>
+
+  <div class="container">
+    <h1>Hola Maquina</h1>
+    <h2>{{ favorito }}</h2>
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body" class="mb-2"
+      @CambiarFavoritoNombre=cambiarFavorito />
+  </div>
+
 </template>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,25 @@
+<script setup>
+//const props = defineProps(['title', 'id', 'body']);
+defineProps({
+    title: String,
+    id: Number,
+    body: {
+
+        type: String,
+        default: 'No hay contenido'
+    }
+})
+const emit = defineEmits(['CambiarFavoritoNombre']);
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"> {{ id }} {{ title }}</h5>
+            <p>{{ body }}</p>
+            <button class="btn btn-outline-primary" @click="emit('CambiarFavoritoNombre', title)">
+                Favorito</button>
+        </div>
+    </div>
+
+</template>
